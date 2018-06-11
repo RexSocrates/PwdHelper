f973d4d01a7edf74dd88f92dc462c27e5514d397
RexSocrates <socrateshung053@gmail.com>
2018-06-11 21:22:28 +0800

    改字

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,9 @@
     </head>
     <body>
         <ul>
-            <li><a href="getEncryptionsList">加密列表</a></li>
-            <li><a href="getDecryptionPage">解密</a></li>
-            <li><a href="randomPwd">隨機產生密碼</a></li>
+            <li><a href="getEncryptionsList">新增密碼</a></li>
+            <li><a href="getDecryptionPage">解密/更新密碼</a></li>
+            <li><a href="randomPwd">產生隨機密碼</a></li>
             <li><a href="changeEncryptionMethod">變更加密方式</a></li>
         </ul>
     </body>
